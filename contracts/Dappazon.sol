01c4d2517701c3bf07446a2a449613334e431c81
Shivshankar Sahu <shivsahu495@gmail.com>
2023-02-15 00:05:42 +0530

    contracts created

--- contracts/Dappazon.sol
+++ contracts/Dappazon.sol
@@ -20,9 +20,29 @@ contract Dappazon {
 
     }
 
+    struct Order{
+        uint256 time;
+        // nested structure of item
+        Item item;
+    }
+
     // mapping to store in key-value pair
     //  unique key is asigned to every item
     mapping(uint256 => Item) public items;
+    mapping(address => uint256) public orderCount;
+    // nested mapping
+    mapping(address => mapping(uint256 => Order)) public orders;
+
+    event Buy(address buyer, uint256 orderId, uint256 itemId);
+    event List(string name, uint256 cost, uint256 quantity);
+
+// it means only owner can call these modifier.
+    modifier onlyOwner() {
+        // true -> means start executing the function
+        // false -> means stop executing the function
+        require(msg.sender == owner);
+        _;
+    }
 
     constructor() {
         // name = "Dappazon";
@@ -38,7 +58,8 @@ contract Dappazon {
     uint256 _cost,
     uint256 _rating,
     uint256 _stock
-    ) public {
+    ) public onlyOwner {
+        
 
         // Create Item struct
         // here Item is a data type
@@ -54,9 +75,43 @@ contract Dappazon {
         //  Save Item struct to blockchain
         items[_id] = item;
 
+        // EMIT an Event
+        // so that you can get push notification or alert an Email for website
+        emit List(_name, _cost, _stock);
+
     }
 
     // Buy products
+    function buy(uint256 _id) public payable {
+        // Receive Crypto
+
+            // Fetch Item
+            Item memory item = items[_id];
+
+            // Require enough ether to buy item
+            require(msg.value >= item.cost);
+
+            // Require item is in stock
+            require(item.stock > 0);
+
+        // Create an Order
+            Order memory order = Order(block.timestamp, item);
+
+            // Save order to chain
+            // Add order for user 
+            orderCount[msg.sender]++; // -- order ID
+            orders[msg.sender][orderCount[msg.sender]] = order;
+
+        // Substract Stock
+        items[_id].stock = item.stock - 1;
+
+        // Emit Event
+        emit Buy(msg.sender, orderCount[msg.sender], item.id);
+    }
 
     // Withdraw funds
+    function withdraw() public onlyOwner {
+        (bool success,) = owner.call{value: address(this).balance}("");
+        require(success);
+    }
 }
--- test/Dappazon.js
+++ test/Dappazon.js
@@ -5,6 +5,15 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
+// global constant for listing an Item
+const ID = 1
+const NAME = "Shoes"
+const CATEGORY = "Clothing"
+const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg"
+const COST = tokens(1)
+const RATING = 4
+const STOCK = 5
+
 describe("Dappazon", () => {
 
   let dappazon;
@@ -36,18 +45,18 @@ describe("Dappazon", () => {
   describe("Listing", ()=> {
     let transaction
 
-    const ID = 1
+   
 
     beforeEach( async () => {
 
      transaction =  await dappazon.connect(deployer).list(
         ID,
-        "Shoes",
-        "Clothing",
-        "IMAGE",
-        1,
-        4,
-        5
+        NAME,
+        CATEGORY,
+        IMAGE,
+        COST,
+        RATING,
+        STOCK
       )
 
       await transaction.wait()
@@ -57,7 +66,106 @@ describe("Dappazon", () => {
       it("Returns item attributes", async () => {
           const item = await dappazon.items(ID)
           expect(item.id).to.equal(ID)
+          expect(item.name).to.equal(NAME)
+          expect(item.category).to.equal(CATEGORY)
+          expect(item.image).to.equal(IMAGE)
+          expect(item.cost).to.equal(COST)
+          expect(item.rating).to.equal(RATING)
+          expect(item.stock).to.equal(STOCK)
+
       })
+
+      it("Emit list event", () => {
+        expect(transaction).to.emit(dappazon, "List");
+      })
+  })
+
+
+  describe("Buying", ()=> {
+    let transaction
+
+   
+
+    beforeEach( async () => {
+      // List an Item
+     transaction =  await dappazon.connect(deployer).list(ID,NAME,CATEGORY,IMAGE,COST,RATING,STOCK)
+
+      await transaction.wait()
+
+      // Buy an Item
+
+      transaction = await dappazon.connect(buyer).buy(ID, {value: COST})
+    
+    })
+
+     
+
+      it("Updates the contract balance",async () => {
+        const result = await ethers.provider.getBalance(dappazon.address)
+        // console.log(result)
+        expect(result).to.equal(COST)
+      })
+
+      it("Updates buyer's order count", async () => {
+        const result = await dappazon.orderCount(buyer.address)
+        expect(result).to.equal(1)
+      })
+
+      it("Add the order", async () => {
+        const order = await dappazon.orders(buyer.address, 1)
+
+        expect(order.time).to.be.greaterThan(0)
+        expect(order.item.name).to.equal(NAME)
+      })
+
+      it("Emit Buy event", () => {
+        expect(transaction).to.emit(dappazon, "Buy")
+      })
+  })
+
+  describe("Withdrawing", ()=> {
+    let balanceBefore
+
+   
+
+    beforeEach( async () => {
+      // List an Item
+     transaction =  await dappazon.connect(deployer).list(ID,NAME,CATEGORY,IMAGE,COST,RATING,STOCK)
+
+      await transaction.wait()
+
+      // Buy an Item
+
+      transaction = await dappazon.connect(buyer).buy(ID, {value: COST})
+      await transaction.wait()
+
+      // Get Deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address)
+
+      // Withdraw
+      transaction = await dappazon.connect(deployer).withdraw()
+      await transaction.wait()
+    })
+
+     
+
+      it("Updates the owner balance",async () => {
+        const balanceAfter = await ethers.provider.getBalance(deployer.address)
+        expect(balanceAfter).to.be.greaterThan(balanceBefore)
+      })
+
+
+      it("Updates the contract balance",async () => {
+        const result = await ethers.provider.getBalance(dappazon.address)
+        // console.log(result)
+        expect(result).to.equal(0)
+      })
+
+     
+
+      
+
+     
   })
 
     
